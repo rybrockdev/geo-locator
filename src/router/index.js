96d977dbb5add87293d9ace49982c331d5780b9c
Ryan Brockley <rybrockers@yahoo.com>
2019-06-15 22:34:21 +0100

    Addinuser auth for login and redirect

--- src/router/index.js
+++ src/router/index.js
@@ -3,15 +3,19 @@ import Router from 'vue-router'
 import GMap from '../components/Home/GMap'
 import Signup from '../components/Auth/Signup'
 import Login from '../components/Auth/Login'
+import firebase from '../../Firebase/init'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   routes: [
     {
       path: '/',
       name: 'GMap',
-      component: GMap
+      component: GMap,
+      meta: {
+        requiresAuth: true
+      }
     },
 
     {
@@ -26,4 +30,20 @@ export default new Router({
       component: Login
     }
   ]
+})
+
+router.beforeEach((to, from, next) => {
+  // check to see if the route requires auth
+  if (to.matched.some(rec => rec.meta.requiresAuth)) {
+    // check if the user has auth
+    let user = firebase.auth().currentUser
+    if (user) {
+      // user has auth, procced to route
+      next()
+    } else {
+      // redirect user without auth to login
+      next({ name: 'Login' })
+
+    }
+  }
 })
